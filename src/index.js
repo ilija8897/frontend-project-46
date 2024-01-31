c526904525be8da05b207e9cb85fc126d46018c9
Никифоров Илья Олегович <IONikiforov@alfabank.ru>
2024-01-31 21:55:05 +0300

    diff functional

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ export const initialCommander = () => {
     .description('Compares two configuration files and shows a difference.')
     .arguments('<filepath1> <filepath2>')
     .action(() => {
-        const firstFile = fs.readFileSync(path.resolve(cwd(), process.argv[2]));
-        const secondFile = fs.readFileSync(path.resolve(cwd(), process.argv[3]));
+        getDiff();
     })
     program.parse();
 }
@@ -19,3 +18,57 @@ export const initialCommander = () => {
 function getFormat(path) {
     return path.split('.').pop();
 };
+
+function getFile(filePath) {
+    return fs.readFileSync(path.resolve(cwd(), filePath));
+};
+
+function getParsedData(data) {
+    return JSON.parse(data);
+}
+const TYPES = {
+    OVER: 'OVER',
+    EXIST: 'EXIST',
+    COLLISION: 'COLLISION',
+};
+
+function getDiff() {
+    const firstFile = getFile(process.argv[2]);
+    const secondFile = getFile(process.argv[3]);
+
+    if (JSON.stringify(firstFile) === JSON.stringify(secondFile)) return firstFile;
+
+    const firstDataParced = getParsedData(firstFile);
+    const secondDataParced = getParsedData(secondFile);
+
+    const unionKeys = Object.keys({ ...firstDataParced, ...secondDataParced }).sort((a, b) => a.localeCompare(b));
+
+    const result = unionKeys.map((key) => {
+        if (!Object.hasOwn(firstDataParced, key)) {
+            return { type: TYPES.OVER, element: { key, value: secondDataParced[key] } }
+        }
+        if (!Object.hasOwn(secondDataParced, key)) {
+            return { type: TYPES.EXIST, element: { key, value: firstDataParced[key] } }
+        }
+        if (firstDataParced[key] && secondDataParced[key]) {
+            return { type: TYPES.COLLISION, element: { key, value: [firstDataParced[key], secondDataParced[key]] } }
+        }
+    })
+
+    const diffString = result.reduce((diffAccum, {type, element}) => {
+        if (type === TYPES.OVER) {
+            return `${diffAccum}\n + ${element.key}: ${element.value}`
+        }
+        if (type === TYPES.EXIST) {
+            return `${diffAccum}\n - ${element.key}: ${element.value}`
+        }
+        if (type === TYPES.COLLISION) {
+            if (element.value[0] !== element.value[1]) {
+                return `${diffAccum}\n - ${element.key}: ${element.value[0]};\n + ${element.key}: ${element.value[1]}`
+            }
+            return `${diffAccum}\n   ${element.key}: ${element.value[0]}`
+        }
+    }, '')
+
+    console.log('{}'.split('').join(`${diffString}\n`));
+}
